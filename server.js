a64c279ff1c9e3b9835d182500a8733de7c051da
Sriniketh S <81572169+sriniketh-cp@users.noreply.github.com>
2026-01-02 20:37:37 +0530

    Refactor nodemailer transport configuration

--- server.js
+++ server.js
@@ -5,22 +5,23 @@ import nodemailer from 'nodemailer'
 
 dotenv.config();
 
-const transporter=nodemailer.createTransport({
-  host:"smtp.gmail.com",
-  port:465,
-  secure:true,
-  auth:{
-    user:process.env.EMAIL_USER,
-    pass:process.env.EMAIL_PASS,
 
-  },
-  connectionTimeout:1000
-})
 const app=express();
 const PORT=process.env.PORT||5000;
 
 app.use(cors())
 app.use(express.json())
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false, // required for 587
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
 
 app.get("/" ,(req,res)=>{
     res.send("Backend is running")
