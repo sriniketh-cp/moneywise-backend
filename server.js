f7dcf8771f75a8cc3b51b325fbaffdd44df40f0d
Sriniketh S <81572169+sriniketh-cp@users.noreply.github.com>
2026-01-02 20:49:59 +0530

    Change email sender address in server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post("/send-email",async (req,res) =>{
 
   try{
     await transporter.sendMail({
-      from:process.env.EMAIL_USER,
+      from:contact@moneywisemag.in,
       to:"sriniketh.sudheendra@gmail.com",
       subject:`New subscription from ${name}`,
       text:`
