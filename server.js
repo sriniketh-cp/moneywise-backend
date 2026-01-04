46ae40b8f16e9005ad706bb4ad3ec0798ce05516
Sriniketh S <81572169+sriniketh-cp@users.noreply.github.com>
2026-01-04 16:26:24 +0530

    Refactor email sending to use Brevo API

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from "dotenv"
-import nodemailer from 'nodemailer'
 
 dotenv.config();
 
@@ -12,21 +11,7 @@ const PORT=process.env.PORT||5000;
 app.use(cors())
 app.use(express.json())
 
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false, // required for 587
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-  tls:{
-    rejectUnauthorized:false
-},
- connectionTimeout:20000,
- greetingTimeout:20000,
- socketTimeout:20000,
-});
+
 
 transporter.verify((err, success) => {
   if (err) {
@@ -47,29 +32,66 @@ app.listen(PORT,()=>{
 })
 
 app.post("/send-email",async (req,res) =>{
-  const {name,email,address,phone,plans}=req.body
+  const {name,email,address,phone,plans}=req.body||{};
 
-  try{
-    await transporter.sendMail({
-      from:"contact@moneywisemag.in",
-      to:"sriniketh.sudheendra@gmail.com",
-      subject:`New subscription from ${name}`,
-      text:`
-      Name:${name},
-      email:${email},
-      Address:${address},
-      Phone:${phone},
-      Plans Selected:${plans.join(",")}
-      `
-    })
-    res.json({success:true,message:"Email sent sucessfully"
-    })
-  }catch(error) {
-    console.error(error)
-    res.status(500).json({sucess:false,message:"Email Failed"})
+  if(!name||!enail!Array.isArray(plans)){
+    return res.status(400).json({
+      sucess:false,
+      message:"Invalid request body",
+    });
   }
 
-  
+  try{
+    const response=await fetch("https://api.brevo.com/v3/smtp/email",
+        method:"POST"
+        headers:{
+          "api-key":process.env.BREVO_API_KEY,
+          "Content-Type":"application/json"
+          "accept":"application/json"
 
+        },
+    body:JSON.stringify({
+      sender:{
+        name:"MoneyWise",
+        email:"contact@moneywisemag.in",
+      },
+      to:[
+        {
+          email:"sriniketh.sudheendra@gmail.com",
+          name:"Sriniketh"
+        },
+        ],
+      subject:`New Subscription from ${name}`
+      textContent:
+      Name:${name}
+      Email:${email}
+      Address:${address}
+      Phone:${phone}
+      Plans Selected:${plans.join(", ")}
+    `,
+    
 
-})
+}),
+});
+
+const result=await response.json();
+
+if(!response.ok){
+console.error("BREVO API ERROR:" ,result);
+return res.status(500).json({
+sucess:false,
+message:"Email failed"
+});
+}
+res.json({
+success:true,
+message:"Email sent sucessfully",
+});
+}catch(error){
+console.error("EMAIL ERROR:",error);
+res.status(500).json({
+success:false,
+message:"Email failed",
+});
+}
+});
