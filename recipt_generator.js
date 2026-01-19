ab36fb4584e0c622e302c67bf8d344e126661c00
sriniketh-cp <sriniketh.sudheendra@gmail.com>
2026-01-19 17:38:36 +0530

    added a recipt generator

---
+++ recipt_generator.js
@@ -0,0 +1,34 @@
+import PDFDocument from "pdfkit"
+import fs from "fs"
+import path from "path"
+
+export function generaterecipt(data) {
+    const doc=new PDFDocument({size:"A4",margin:50})
+
+    const fileName=`recipt-${data.TransactionId}.pdf`
+    const filePath=path.join("recipts",fileName);
+
+    doc.pipe(fs.createWriteStream(filePath))
+
+    doc.fontSize(20).text("Payment Recipt",{align:"center"})
+    doc.moveDown();
+
+    doc.fontSize(12);
+    doc.text(`Name:${data.name}`)
+    doc.text(`Email:${data.email}`)
+    doc.text(`Phone:${data.phone}`)
+    doc.text(`Transaction ID:${data.TransactionId}`)
+    doc.text(`Plan(s):${data.plans.join(", ")}`)
+    doc.text(`Date: ${new Date().toLocaleDateString()}`);
+
+    doc.moveDown();
+    doc.text("Thank you for subscribing to Moneywise!",
+        {
+            align:"center"
+        }
+    )
+    doc.end();
+
+    return fileName;
+
+}
--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from "dotenv"
+import nodemailer from 'nodemailer'
+import { generaterecipt } from './recipt_generator';
 
 dotenv.config();
 
 const app=express();
 const PORT=process.env.PORT||5000;
 
+// Create email transporter
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
 app.use(cors())
 app.use(express.json())
+
+app.use("/receipts", express.static("receipts"));
+
 
 app.get("/" ,(req,res)=>{
     res.send("Backend is running")
@@ -19,29 +33,29 @@ app.listen(PORT,()=>{
 })
 
 app.post("/send-email",async (req,res) =>{
-  const {name,email,address,phone,plans}=req.body
-
+  const {name,email,address,phone,TransactionId,plans}=req.body
+  
   try{
+    const reciptfile=generaterecipt(req.body)
+    
     await transporter.sendMail({
       from:process.env.EMAIL_USER,
       to:"sriniketh.sudheendra@gmail.com",
       subject:`New subscription from ${name}`,
       text:`
-      Name:${name},
-      email:${email},
-      Address:${address},
-      Phone:${phone},
-      Plans Selected:${plans.join(",")}
+      Name: ${name}
+      Email: ${email}
+      Address: ${address}
+      Phone: ${phone}
+      Transaction ID: ${TransactionId}
+      Plans Selected: ${plans.join(", ")}
       `
     })
-    res.json({success:true,message:"Email sent sucessfully"
+    res.json({success:true,message:"Email sent sucessfully",
+      reciptUrl:`https://moneywisemag.in/recipts/${reciptfile}`
     })
   }catch(error) {
-    console.error(error)
-    res.status(500).json({sucess:false,message:"Email Failed"})
+    console.error("Email Error:", error)
+    res.status(500).json({success:false,message:"Email Failed", error: error.message})
   }
-
-  
-
-
 })
